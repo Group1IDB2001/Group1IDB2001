dcbcc614311b31922a53aeeba66bf2ecaa5d15d5
Group1IDB2001 <103366213+Group1IDB2001@users.noreply.github.com>
2022-04-13 17:30:40 +0300

    Year

--- Project_3/Models/Entity/Year.cs
+++ Project_3/Models/Entity/Year.cs
@@ -2,5 +2,10 @@
 {
     public class Year
     {
+        public int year { get; set; }
+
+        public List<Guid> AuthorID = new List<Guid>() { Guid.Parse("00000000-0000-0000-0000-00000000") };
+
+        public List<int> BookID = new List<int>() { 0 };
     }
 }
