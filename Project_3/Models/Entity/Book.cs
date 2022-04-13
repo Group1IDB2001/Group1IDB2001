84622391008bdbd144994970424aa08768d10454
Group1IDB2001 <103366213+Group1IDB2001@users.noreply.github.com>
2022-04-13 17:31:08 +0300

    Author guid

--- Project_3/Models/Entity/Book.cs
+++ Project_3/Models/Entity/Book.cs
@@ -20,6 +20,63 @@ namespace Project_3.Models.Entity
         public int? DataID  { get; set; }
         public string? aboutBook { get; set; }
 
+<<<<<<< Updated upstream
+=======
+        public IEnumerable<Book> ShowTenBooks(int PageNumber) //  Возвращает 1 страницу (10 обьектов Book) из всего каталога с указанным номером
+        {
+            IEnumerable < Book > Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SelectBooksByAuthor (int PageNumber,int AuthorID) // Возвращает 1 страницу Book с указанным AuthorID
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SelectBooksByGenre(int PageNumber, int GenreID) // Возвращает 1 страницу Book с указанным GenreID
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SelectBooksByType(int PageNumber, int TypeID) // Возвращает 1 страницу Book с указанным TypeID
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SelectBooksByLanguage(int PageNumber, int LanguageID) // Возвращает 1 страницу Book с указанным LanguageID
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SelectBooksByCountry(int PageNumber, int CountryID) // Возвращает 1 страницу Book с указанным CountryID
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SortBooksByYearAscending(int PageNumber) // Возвращает 1 страницу Book , отсортированных по году издания в порядке возрастания
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SortBooksByYearDescending(int PageNumber) // Возвращает 1 страницу Book , отсортированных по году издания в порядке убывания
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+        public IEnumerable<Book> SearchBookByName(int PageNumber,string Name) // Возвращает 1 страницу Book , имя которых содержит заданную строку
+        {
+            IEnumerable<Book> Page = new List<Book>();
+            return Page;
+        }
+
+>>>>>>> Stashed changes
 
     }
 }
