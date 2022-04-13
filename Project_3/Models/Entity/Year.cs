e1528f1213f6f73c59ee13d48f8cf5a49f9b1ce4
Group1IDB2001 <project3afa@gmail.com>
2022-04-13 15:28:59 +0100

    guid 2

--- Project_3/Models/Entity/Year.cs
+++ Project_3/Models/Entity/Year.cs
@@ -2,10 +2,5 @@
 {
     public class Year
     {
-        public int year { get; set; }
-
-        public List<int> AuthorID = new List<int>() { 0 };
-
-        public List<int> BookID = new List<int>() { 0 };
     }
 }
