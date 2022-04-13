60e4748aebf7675d9ed56efc58bc6d53df240254
Group1IDB2001 <project3afa@gmail.com>
2022-04-13 14:11:49 +0100

    исправил функции

--- Project_3/Controllers/BooksController.cs
+++ Project_3/Controllers/BooksController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Project_3.Managers.Books;
 
 namespace Project_3.Controllers
 {
     public class BooksController : Controller
     {
+        private IBookManager _manager;
+
+        public BooksController(IBookManager manager)
+        {
+            _manager = manager;
+        }
+
+
         public IActionResult Index()
         {
+            var books =
             return View();
         }
+
+
     }
 }
