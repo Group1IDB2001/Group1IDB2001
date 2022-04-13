edae12551e8835d4be3fc49111002be32fdc9a3c
Group1IDB2001 <project3afa@gmail.com>
2022-04-13 15:27:08 +0100

    Guid

--- Project_3/Models/Entity/Type.cs
+++ Project_3/Models/Entity/Type.cs
@@ -2,10 +2,10 @@
 {
     public class Type
     {
-        public int TypeID { get; set; }
+        public Guid TypeID { get; set; }
         public string TypeName { get; set; }
 
-        public List<int> AuthorID = new List<int>() { 0 };
+        public List<Guid> AuthorID = new List<Guid>() { Guid.Parse("00000000-0000-0000-0000-00000000") };
 
         public List<int> BookID = new List<int>() { 0 };
     }
